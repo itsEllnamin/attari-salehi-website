faf5236817ecf0f246c1c5d88f63b7c27b6e7cb6
itsEllnamin <136508538+itsEllnamin@users.noreply.github.com>
2023-08-21 16:10:31 +0330

    Update settings.py
    MERCHANT edited

--- attari/settings.py
+++ attari/settings.py
@@ -229,5 +229,5 @@ CKEDITOR_CONFIGS = {
 # Zarinpal
 # https://www.zarinpal.com/
 
-MERCHANT  =  "3454475f-ceba-4da1-8eeb-e7bb3133ae2b"
+MERCHANT  =  "00000000-0000-0000-0000-000000000000"
 SANDBOX  =  True
