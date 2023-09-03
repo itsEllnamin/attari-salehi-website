69f3aa01e9a75e1a56d20a382dc87406246853ff
itsEllnamin <136508538+itsEllnamin@users.noreply.github.com>
2023-09-03 15:40:33 +0330

    nothing special

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin,
@@ -9,6 +12,64 @@ from django.utils import timezone
 from django.core.validators import RegexValidator
 from utils import FileManager
 
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, mobile_number, password, activation_code=None, email='', name='', family=''):
+        if not mobile_number:
+            raise ValueError('شماره موبایل باید وارد شود.')
+        
+        user = self.model(
+            mobile_number = mobile_number,
+            activation_code = activation_code,
+            email = self.normalize_email(email),
+            name = name,
+            family = family,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, mobile_number, password, email, name, family):
+        user = self.create_user(
+            mobile_number,
+            password,
+            email,
+            name,
+            family
+        )
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    mobile_number = models.CharField(_('شماره موبایل'), max_length=11, unique=True)
+    name = models.CharField(_('نام'), max_length=50, blank=True)
+    family = models.CharField(_('نام‌خانوادگی'), max_length=50, blank=True)
+    email = models.EmailField(_('ایمیل'), blank=True)
+    is_staff = models.BooleanField(_('وضعیت ادمینی'), default=False)
+    is_active = models.BooleanField(_('وضعیت فعال/غیرفعال'), default=False)
+    datetime_joined = models.DateTimeField(_('تاریخ ثبت‌نام'), default=timezone.now)
+    activation_code = models.CharField(_('کد فعالسازی'), max_length=100, null=True, blank=True)
+
+    USERNAME_FIELD = 'mobile_number'
+    REQUIRED_FIELDS = ['email', 'name', 'family']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.name+' '+self.family
+    
+    @property
+    def is_staff(self):
+        return self.is_staff
+
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
 
 
 class CustomUserManager(BaseUserManager):
